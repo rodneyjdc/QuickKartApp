fc65ee90f58de341a55a5d46d5438eadbfc990d6
Rodney Canlas <canlasrj@gmail.com>
2020-07-08 14:54:25 -0700

    Added a GetCartDetails method in Cart class.

---
+++ QuickKartApp/Library/Models/Cart.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace Library.Models
+{
+    public class Cart
+    {
+        public ArrayList Products { get; set; }
+
+        public Cart()
+        {
+            this.Products = new ArrayList();
+        }
+
+        public void AddToCart(Product product)
+        {
+            this.Products.Add(product);
+        }
+
+        public void RemoveFromCart(Product productToRemove)
+        {
+            this.Products.Remove(productToRemove);
+        }
+
+        public ArrayList GetCartProductDetails()
+        {
+            return this.Products;
+        }
+
+        public string GetCartDetails()
+        {
+            int capacity = this.Products.Capacity;
+            int count = this.Products.Count;
+
+            string details = string.Empty;
+            details += "Cart Capacity = " + capacity;
+            details += "\nProducts Count = " + count;
+
+            return details;
+        }
+    }
+}
